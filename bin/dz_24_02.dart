65cf8fd508ea6e2cb3bcd81af84881b03b3f702c
Kasym0101 <kasym.dzhorobaev@gmail.com>
2023-02-27 19:49:58 +0600

    first commit

---
+++ bin/dz_24_02.dart
@@ -0,0 +1,73 @@
+void main() {
+  List a = [1, 7, 12, 3, 56, 2, 87, 34, 54];
+  print(a.first);
+  print(a[4]);
+  print(a.last);
+}
+
+
+// void main() {
+//   List a = [3, 12, 43, 1, 25, 6, 5, 7];
+//   List b = [55, 11, 23, 56, 78, 1, 9];
+//   print(a + b);
+// }
+
+
+// void main() {
+//   List a = [
+//     'a',
+//     'd',
+//     'F',
+//     'l',
+//     'u',
+//     't',
+//     't',
+//     'e',
+//     'R',
+//     'y',
+//     '3',
+//     'b',
+//     'h',
+//     'j'
+//   ];
+//   a.removeRange(0, 2);
+//   a.removeRange(7, 12);
+//   print(a);
+// }
+
+
+// void main() {
+//   List a = [1, 2, 3, 4, 5, 6, 7];
+//   print(a.contains(3));
+//   print(a.first);
+//   print(a.last);
+//   print(a.length);
+// }
+
+
+// void main() {
+//   List a = [601123, 2, "dart", 45, 95, "dart24", 1];
+//   print(a.contains("dart"));
+//   print(a.contains(951));
+// }
+
+
+// void main() {
+//   List a = ['post', 1, 0, 'flutter'];
+//   String myDart = 'Flutter';
+//   print(myDart.isNotEmpty);
+// }
+
+
+// void main() {
+//   List a = ['I', 'Started', 'Learn', 'Flutter', 'Since', 'April'];
+//   String myFlutter = a.join('*');
+//   print(myFlutter);
+// }
+
+
+// void main() {
+//   List a = [1, 9, 3, 195, 202, 2, 5, 7, 9, 10, 3, 15, 0, 11];
+//   a.sort();
+//   print(a);
+// }
